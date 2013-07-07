a62b189a10cf6a4c22647c1fe51c98d7226f6b54
Eric Mill <konklone@gmail.com>
2013-07-07 18:45:34 -0400

    Limit Twilio to using a short message

--- fisa.rb
+++ fisa.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-
 require 'yaml'
 require 'fileutils'
 require 'open-uri'
-require 'git'
 
-# communication mechanisms
+# install these gems:
+require 'git'
 require 'twitter'
 require 'pony'
 require 'twilio-rb'
@@ -30,7 +29,7 @@ if config['twitter']
   end
 end
 
-if config["twilio"]
+if config['twilio']
   Twilio::Config.setup(
     account_sid: config['twilio']['account_sid'],
     auth_token: config['twilio']['auth_token']
@@ -66,10 +65,10 @@ def check_fisa
 end
 
 # notify the admin and/or the world about it
-def notify_fisa(msg)
-  Twitter.update(msg) if config['twitter']
-  Pony.mail(config['email'].merge(body: msg)) if config['email']
-  Twilio::SMS.create(to: config['twilio']['to'], from: config['twilio']['from'], body: msg) if config['twilio']
+def notify_fisa(long_msg, short_msg)
+  Twitter.update(long_msg) if config['twitter']
+  Pony.mail(config['email'].merge(body: long_msg)) if config['email']
+  Twilio::SMS.create(to: config['twilio']['to'], from: config['twilio']['from'], body: short_msg) if config['twilio']
 
   puts "Notified: #{msg}"
 end
@@ -80,12 +79,13 @@ end
 
 if sha = check_fisa
   url = "http://www.uscourts.gov/uscourts/courts/fisc/index.html"
-  msg = "Just updated with something! #{url}"
+  short_msg = "Just updated with something!\n#{url}"
+  long_msg = short_msg.dup
 
   if config['github']
-    diff_url = "https://github.com/#{config['github']['repo']}/commit/#{sha}"
-    msg += " Here's a diff of what changed: #{diff_url}"
+    diff_url = "https://github.com/#{config['github']}/commit/#{sha}"
+    long_msg += "\n\nLine-by-line breakdown of what changed:\n#{diff_url}"
   end
 
-  notify_fisa msg
+  notify_fisa long_msg, short_msg
 end
