644d3cc0836ad31d4f52b8e6e2bd50295c76ab27
Eric Mill <eric@konklone.com>
2013-11-04 19:41:22 -0500

    using a separate branch from master for fisa.html from now on

--- fisa.rb
+++ fisa.rb
@@ -16,9 +16,8 @@ require 'xmlsimple'
 # docket detection (optional)
 require './dockets'
 
+# working directory should always be next to this script, to read in config.yml
 FileUtils.chdir File.dirname(__FILE__)
-
-@git = Git.open './'
 
 def config
   @config ||= YAML.load(File.read("config.yml"))
@@ -47,84 +46,93 @@ if config['pushover']
   end
 end
 
+# ensure git repo/branch for docket is checked out and ready
+# assumptions: remote exists, and branch exists on remote
+@docket = "docket"
+if !File.exists?(@docket)
+  puts "[git] Cloning docket branch..."
+  system "git clone --branch #{config['docket']['branch']} --single-branch #{config['docket']['remote']} #{@docket}"
+end
+puts "[git] Switching to docket branch..."
+system "cd #{@docket} && git checkout #{config['docket']['branch']}"
+puts "[git] Pulling latest changes..."
+system "cd #{@docket} && git pull --no-edit"
+@git = Git.open @docket
+
+
 # check FISA court for updates, compare to last check
-def check_fisa(test: false, test_error: false, test_file: false)
+def check_fisa(test: false, test_error: false, use_file: false)
   return "test" if test
 
-  puts "Pulling latest changes..."
-  system "git pull --no-edit" # make sure local branch is tracking a remote!
-
-  if test_file
-    fisa_uri = "./test-fisa.html"
+  if use_file
+    body = File.read "./test-fisa.html"
   else
-    fisa_uri = "http://www.uscourts.gov/uscourts/courts/fisc/index.html?t=#{Time.now.to_i}"
+    puts "Downloading FISC docket..."
+    body = open(
+      "http://www.uscourts.gov/uscourts/courts/fisc/index.html?t=#{Time.now.to_i}",
+      "User-Agent" => "@FISACourt, twitter.com/FISACourt, github.com/konklone/fisa"
+    ).read
   end
 
-  puts "Downloading FISC docket..."
-  open(
-    fisa_uri,
-    "User-Agent" => "@FISACourt, twitter.com/FISACourt, github.com/konklone/fisa"
-  ) do |uri|
-    open("fisa.html", "wt") do |file|
-      file.write uri.read
-      file.close
-    end
+  open("#{@docket}/fisa.html", "wt") do |file|
+    file.write body
+    file.close
+  end
 
-    puts "Saved current state of FISC docket."
+  puts "Saved current state of FISC docket."
 
-    if changed? or test_error
+  if changed? or test_error
+    begin
+
+      # before we accept the changes, parse the diff to figure out
+      # which docket(s) got updated. this is totally optional:
+      # if it fails for any reason, move on.
       begin
-
-        # before we accept the changes, parse the diff to figure out
-        # which docket(s) got updated. this is totally optional:
-        # if it fails for any reason, move on.
-        begin
-          dockets = Dockets.changed(@git)
-          puts "Dockets updated: #{dockets.inspect}"
-        rescue Exception => ex
-          dockets = []
-          puts "Error detecting which docket got changed, moving on."
-        end
-
-        message = "FISC dockets have been updated"
-        if dockets.any?
-          message << ": #{dockets.join ', '}"
-        end
-
-        puts "Committing with message: #{message}"
-        return "test" if test_file # stop here if we're testing the file
-
-        @git.add "fisa.html"
-
-        response = @git.commit message
-        sha = @git.gcommit(response.split(/[ \[\]]/)[2]).sha
-        puts "[#{sha}] Committed update"
-
-        system "git push"
-        puts "[#{sha}] Pushed changes."
-
-        # test error path
-        raise Exception.new("Fake git error!") if test_error
-
-        sha
+        dockets = Dockets.changed(@git)
+        puts "Dockets updated: #{dockets.inspect}"
       rescue Exception => ex
-        puts "Error doing the git commit and push!"
-        puts "Emailing admin, notifying public without SHA."
-        puts
-        puts ex.inspect
-
-        msg = "Git error!"
-        Pony.mail(config['email'].merge(body: msg)) if config['email']
-        Twilio::SMS.create(to: config['twilio']['to'], from: config['twilio']['from'], body: msg) if config['twilio']
-        Pushover.notification(title: msg, message: msg) if config['pushover']
-
-        true
+        dockets = []
+        puts "Error detecting which docket got changed, moving on."
       end
-    else
-      puts "Nothing changed."
-      false
+
+      message = "FISC dockets have been updated"
+      if dockets.any?
+        message << ": #{dockets.join ', '}"
+      end
+
+      puts "Committing with message: #{message}"
+
+      @git.add "fisa.html"
+
+      response = @git.commit message
+      sha = @git.gcommit(response.split(/[ \[\]]/)[2]).sha
+      puts "[#{sha}] Committed update"
+
+      system "cd #{@docket} && git push"
+      puts "[#{sha}] Pushed changes."
+
+      # test error path
+      raise Exception.new("Fake git error!") if test_error
+
+      sha
+    rescue Exception => ex
+      puts "Error doing the git commit and push!"
+      puts "Emailing admin, notifying public without SHA."
+      puts
+      puts ex.inspect
+
+      msg = "Git error!"
+      Pony.mail(config['email'].merge(body: msg)) if config['email']
+      Twilio::SMS.create(to: config['twilio']['to'], from: config['twilio']['from'], body: msg) if config['twilio']
+      Pushover.notification(title: msg, message: msg) if config['pushover']
+
+      true
     end
+  else
+    puts "Nothing changed."
+    false
   end
+
 end
 
 # notify the admin and/or the world about it
@@ -143,7 +151,7 @@ def changed?
   @git.diff('HEAD','fisa.html').entries.length != 0
 end
 
-if sha = check_fisa(test: (ARGV[0] == "test"), test_error: (ARGV[0] == "test_error"), test_file: (ARGV[0] == "test_file"))
+if sha = check_fisa(test: (ARGV[0] == "test"), test_error: (ARGV[0] == "test_error"), use_file: (ARGV[0] == "use_file"))
   url = "http://www.uscourts.gov/uscourts/courts/fisc/index.html"
   short_msg = "Just updated with something!\n#{url}"
   long_msg = short_msg.dup
