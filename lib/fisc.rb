ac568f3790c91f277dc775a96618942555c42733
Eric Mill <eric@konklone.com>
2014-05-25 18:29:55 -0400

    Getting page number

--- lib/fisc.rb
+++ lib/fisc.rb
@@ -1,29 +1,32 @@
-require 'safe_yaml'
 require 'open-uri'
+require 'nokogiri'
 
 require './lib/alerts'
-require "./lib/git"
+require './lib/git'
+require './lib/filings'
 
 module FISC
   URL = "http://www.fisc.uscourts.gov/public-filings"
 
   def self.config
-    @config ||= YAML.safe_load(File.read('config.yml'))
+    @config ||= YAML.load(File.read('config.yml'))
   end
 
-  def self.url_for(page)
-    # includes both docket activity and correspondence
-    url = URL
-    url << "?field_case_reference_nid=All"
-    url << "&page=#{page}"
-    url << "&t=#{Time.now.to_i}"
-    url
+  # use the "last" link to figure out the final page #
+  # pretty brittle: it'd better be there
+  def self.last_page!
+    puts "Finding page number of final page..."
+    first = download! FISC::Filings.url_for(page: 1)
+    doc = Nokogiri::HTML first
+    link = doc.at("li.pager-last").at("a")['href']
+    page = link.scan(/page=(\d+)/).first.first.to_i
+    puts "Last page: #{page}"
+    page
   end
 
 
-  def self.download!(page: 1)
-    url = url_for page
-    puts "Downloading: #{url}"
+  def self.download!(url)
+    # puts "Downloading: #{url}"
     open(
       url,
       "User-Agent" => "@FISACourt, twitter.com/FISACourt, github.com/konklone/fisacourt"
@@ -33,15 +36,27 @@ module FISC
   def self.check!(options: {})
     return "test" if options[:test]
 
-    if options[:use_file]
-      body = File.read "./test/filings.html"
-    else
-      body = download!
-    end
+    pages = options[:archive] ? (1..last_page!).to_a : [1]
 
-    # TODO: replace writing filing HTML to updating parsed data
-    # filings_from_body! body
-    File.open("docket/filings.html", "w") {|file| file.write body}
+    pages.each do |page|
+      puts "[#{page}] Downloading filings..."
+      if options[:use_file]
+        body = File.read "./test/filings#{page}.html"
+      else
+        url = FISC::Filings.url_for page: page
+        body = download! url
+      end
+
+      # parse filing data out of the HTML
+      filings = FISC::Filings.for_page body
+
+      puts filings
+
+      # save a .yml file for each one into the docket dir
+      # filings.each do |filing|
+      #   FISC::Filings.save! filing
+      # end
+    end
 
     puts "Saved current state of FISC docket."
 
@@ -49,10 +64,10 @@ module FISC
       begin
         raise Exception.new("Fake git error!") if test_error
         FISC::Git.save! "FISC dockets have been updated"
+
       rescue Exception => ex
         puts "Error doing the git commit and push! #{ex.inspect}"
         FISC::Alerts.admin! "Git error!"
-
         true
       end
     else
